8a752a7bb32462c4936cee285e176f4be534eb6b
Angel Baro <angelbaro44@gmail.com>
2021-02-12 15:00:54 -0500

    Added Test Cases

--- index.js
+++ index.js
@@ -8,3 +8,28 @@ var moveZeros = function (arr) {
       return newArr.concat(zArr)
       }
       
+
+//  Test Cases:
+
+      console.log(moveZeros([1,2,0,1,0,1,0,3,0,1])); 
+      // Expected Result: [ 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 ])
+
+    
+
+      console.log(moveZeros([9,0.0,0,9,1,2,0,1,0,1,0.0,3,0,1,9,0,0,0,0,9])); 
+      // Expected Result: [9,9,1,2,1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
+
+    
+
+      console.log(moveZeros(["a",0,0,"b","c","d",0,1,0,1,0,3,0,1,9,0,0,0,0,9])); 
+      // Expected Result: ["a","b","c","d",1,1,3,1,9,9,0,0,0,0,0,0,0,0,0,0])
+
+      
+
+      console.log(moveZeros(["a",0,0,"b",null,"c","d",0,1,false,0,1,0,3,[],0,1,9,0,0,{},0,0,9])); 
+      // Expected Result: ["a","b",null,"c","d",1,false,1,3,[],1,9,{},9,0,0,0,0,0,0,0,0,0,0])
+
+    
+
+      console.log(moveZeros([0,1,null,2,false,1,0])); 
+      // Expected Result: [1,null,2,false,1,0,0])
